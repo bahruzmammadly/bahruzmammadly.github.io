9ddb6e3aba74359c0d885bc8ef3ba6b37070b1b2
user <user@localhost>
2026-02-13 22:36:34 +0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,20 @@
 const nextConfig = {
   output: 'export',
   typescript: { ignoreBuildErrors: true },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
